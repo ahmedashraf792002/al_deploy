f30c322905f67590d9cd27d3d6e8067a5f71e3c3
Ahmed Ashraf <117744931+ahmedashraf792002@users.noreply.github.com>
2024-01-26 00:55:05 +0200

    Update al_deploy.py

--- al_deploy.py
+++ al_deploy.py
@@ -8,7 +8,7 @@ from PIL import Image
 from keras.applications.vgg16 import preprocess_input
 # Load your trained model
 
-MODEL_PATH = r'd:\project documentation\alzheimers models\deploy\new folder\al_deploy\CNN_model.h5'
+MODEL_PATH = 'CNN_model.h5'
 model = load_model(MODEL_PATH)
 
 # Function for processing the input image and prediction
@@ -24,7 +24,7 @@ def model_predict(img_path, model):
 
 # Main Streamlit app
 def main():
-    with open(r'd:\project documentation\alzheimers models\deploy\new folder\al_deploy\design.css') as source_des:
+    with open('design.css') as source_des:
         st.markdown(f"<style>{source_des.read()}</style>",unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center;'>Alzheimer's Disease Diagnosis App</h1>",unsafe_allow_html=True)
     #st.title('''Alzheimer's Disease Diagnosis App''')
